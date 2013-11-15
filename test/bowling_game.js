e8cf2fbb77e0baabde4a2ba04d581226d3908bed
Matthew Thorley <padwasabimasala@gmail>
2013-11-15 10:28:05 -0700

    second test
    Already passes. This probably should have just been the first test

--- test/bowling_game.js
+++ test/bowling_game.js
@@ -2,3 +2,10 @@ var chai = require('chai')
 chai.should()
 
 var BowlingGame = require ('../bowling_game').BowlingGame
+
+describe('new BowlingGame()', function() {
+  it('should create a new BolwingGame', function() {
+    game = new BowlingGame
+    game.should.be.a('Object')
+  })
+})
